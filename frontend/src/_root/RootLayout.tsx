cf317cd1613d52697ee69582261ce09d0cfaf5e9
shedrack <112991289+shedsoncode0@users.noreply.github.com>
2026-02-17 04:40:53 +0100

    Update RootLayout.tsx

--- frontend/src/_root/RootLayout.tsx
+++ frontend/src/_root/RootLayout.tsx
@@ -183,6 +183,7 @@ const RootLayout = () => {
                     key={item.name}
                     as="a"
                     href={item.href}
+                    onClick={item.onClick}
                     className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white"
                   >
                     {item.name}
